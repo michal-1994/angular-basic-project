bc54298de570b595dfab2be3866cadab77dd3e36
Michal <michalgrzegorczyk04@gmail.com>
2022-02-26 12:22:37 +0100

    Add dependecny injection

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,6 +1,7 @@
-import { Component, OnInit, EventEmitter, Output } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 import { Recipe } from '../recipe.model';
+import { RecipeService } from '../recipe.service';
 
 @Component({
   selector: 'app-recipe-list',
@@ -8,19 +9,11 @@ import { Recipe } from '../recipe.model';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
-  @Output() recipeWasSelected = new EventEmitter<Recipe>();
-  recipes: Recipe[] = [
-    new Recipe('A test Recipe - 1', 'This is simply a test - 1', 'https://images.pexels.com/photos/3771106/pexels-photo-3771106.jpeg?auto=compress&cs=tinysrgb&h=750&w=1260'),
-    new Recipe('A test Recipe - 2', 'This is simply a test - 2', 'https://images.pexels.com/photos/1109197/pexels-photo-1109197.jpeg?auto=compress&cs=tinysrgb&h=650&w=940')
-  ];
+  recipes: Recipe[];
 
-  constructor() { }
+  constructor(private recipeService: RecipeService) { }
 
   ngOnInit(): void {
+    this.recipes = this.recipeService.getRecipes();
   }
-
-  onRecipeSelected(recipe: Recipe) {
-    this.recipeWasSelected.emit(recipe);
-  }
-
 }
---
+++ src/app/recipes/recipe.service.ts
@@ -0,0 +1,41 @@
+import { EventEmitter, Injectable } from '@angular/core';
+import { Recipe } from './recipe.model';
+
+import { Ingredient } from '../shared/ingredient.model';
+import { ShoppingListService } from '../shopping-list/shopping-list.service';
+
+@Injectable()
+export class RecipeService {
+    recipeSelected = new EventEmitter<Recipe>();
+
+    private recipes: Recipe[] = [
+        new Recipe(
+            'Tasty Schnitzel',
+            'A super tasty Schnitzel - just awesome!',
+            'https://images.pexels.com/photos/1352274/pexels-photo-1352274.jpeg?auto=compress&cs=tinysrgb&h=650&w=940',
+            [
+                new Ingredient('Meat', 1),
+                new Ingredient('Fries', 20)
+            ]
+        ),
+        new Recipe(
+            'Big fat burger',
+            'What else you need to say?',
+            'https://images.pexels.com/photos/1639562/pexels-photo-1639562.jpeg?auto=compress&cs=tinysrgb&h=650&w=940',
+            [
+                new Ingredient('Buns', 2),
+                new Ingredient('Meat', 1)
+            ]
+        )
+    ];
+
+    constructor(private slService: ShoppingListService) {}
+
+    getRecipes() {
+        return this.recipes.slice();
+    }
+
+    addIngredientsToShoppingList(ingredients: Ingredient[]) {
+        this.slService.addIngredients(ingredients);
+    }
+}
