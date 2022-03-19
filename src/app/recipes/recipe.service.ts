7d96b9d9cf9e81b4bb01adaf0e1d6f6d1bd682c0
Michal <michalgrzegorczyk04@gmail.com>
2022-03-19 10:45:28 +0100

    Add http fetch data for project

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,43 +1,47 @@
 import { Injectable } from '@angular/core';
-import { Recipe } from './recipe.model';
+import { Subject } from 'rxjs';
 
+import { Recipe } from './recipe.model';
 import { Ingredient } from '../shared/ingredient.model';
 import { ShoppingListService } from '../shopping-list/shopping-list.service';
-import { Subject } from 'rxjs';
+import { DataStorageService } from '../shared/data-storage.service';
 
 @Injectable()
 export class RecipeService {
     recipesChanged = new Subject<Recipe[]>();
 
-    private recipes: Recipe[] = [
-        new Recipe(
-            'Tasty Schnitzel',
-            'A super tasty Schnitzel - just awesome!',
-            'https://images.pexels.com/photos/1352274/pexels-photo-1352274.jpeg?auto=compress&cs=tinysrgb&h=650&w=940',
-            [
-                new Ingredient('Meat', 1),
-                new Ingredient('Fries', 20)
-            ]
-        ),
-        new Recipe(
-            'Big fat burger',
-            'What else you need to say?',
-            'https://images.pexels.com/photos/1639562/pexels-photo-1639562.jpeg?auto=compress&cs=tinysrgb&h=650&w=940',
-            [
-                new Ingredient('Buns', 2),
-                new Ingredient('Meat', 1)
-            ]
-        )
-    ];
+    // private recipes: Recipe[] = [
+    //   new Recipe(
+    //     'Tasty Schnitzel',
+    //     'A super-tasty Schnitzel - just awesome!',
+    //     'https://upload.wikimedia.org/wikipedia/commons/7/72/Schnitzel.JPG',
+    //     [new Ingredient('Meat', 1), new Ingredient('French Fries', 20)]
+    //   ),
+    //   new Recipe(
+    //     'Big Fat Burger',
+    //     'What else you need to say?',
+    //     'https://upload.wikimedia.org/wikipedia/commons/b/be/Burger_King_Angus_Bacon_%26_Cheese_Steak_Burger.jpg',
+    //     [new Ingredient('Buns', 2), new Ingredient('Meat', 1)]
+    //   )
+    // ];
 
-    constructor(private slService: ShoppingListService) {}
+    private recipes: Recipe[] = [];
+
+    constructor(
+        private slService: ShoppingListService
+    ) {}
+
+    setRecipes(recipes: Recipe[]) {
+        this.recipes = recipes;
+        this.recipesChanged.next(this.recipes.slice());
+    }
 
     getRecipes() {
         return this.recipes.slice();
     }
 
     getRecipe(index: number) {
-        return this.recipes.slice()[index];
+        return this.recipes[index];
     }
 
     addIngredientsToShoppingList(ingredients: Ingredient[]) {
